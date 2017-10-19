1d7560fad9a27b8002758ad75bbe6945cb9cca46
William Huang <william.huang03@outlook.com>
2017-10-19 17:12:07 -0400

    Added macro definitions to GPIO driver

--- Drivers/HAL/Include/HAL_GPIO_Driver.h
+++ Drivers/HAL/Include/HAL_GPIO_Driver.h
@@ -11,45 +11,51 @@ extern "C" {
 /*****************************************************************************/
 /*****************************************************************************/
 /*****************************************************************************/
-#define GPIO_PIN_0      (1U << 0)
-#define GPIO_PIN_1      (1U << 1)
-#define GPIO_PIN_2      (1U << 2)
-#define GPIO_PIN_3      (1U << 3)
-#define GPIO_PIN_4      (1U << 4)
-#define GPIO_PIN_5      (1U << 5)
-#define GPIO_PIN_6      (1U << 6)
-#define GPIO_PIN_7      (1U << 7)
-#define GPIO_PIN_8      (1U << 8)
-#define GPIO_PIN_9      (1U << 9)
-#define GPIO_PIN_10     (1U << 10)
-#define GPIO_PIN_11     (1U << 11)
-#define GPIO_PIN_12     (1U << 12)
-#define GPIO_PIN_13     (1U << 13)
-#define GPIO_PIN_14     (1U << 14)
-#define GPIO_PIN_15     (1U << 15)
-#define GPIO_PIN_ALL    (0xFF)
-#define GPIO_MASK
+#define GPIO_PIN_0      ((uint16_t)0x0001)  /*!< Pin 0 selected    */
+#define GPIO_PIN_1      ((uint16_t)0x0002)  /*!< Pin 1 selected    */
+#define GPIO_PIN_2      ((uint16_t)0x0004)  /*!< Pin 2 selected    */
+#define GPIO_PIN_3      ((uint16_t)0x0008)  /*!< Pin 3 selected    */
+#define GPIO_PIN_4      ((uint16_t)0x0010)  /*!< Pin 4 selected    */
+#define GPIO_PIN_5      ((uint16_t)0x0020)  /*!< Pin 5 selected    */
+#define GPIO_PIN_6      ((uint16_t)0x0040)  /*!< Pin 6 selected    */
+#define GPIO_PIN_7      ((uint16_t)0x0080)  /*!< Pin 7 selected    */
+#define GPIO_PIN_8      ((uint16_t)0x0100)  /*!< Pin 8 selected    */
+#define GPIO_PIN_9      ((uint16_t)0x0200)  /*!< Pin 9 selected    */
+#define GPIO_PIN_10     ((uint16_t)0x0400)  /*!< Pin 10 selected   */
+#define GPIO_PIN_11     ((uint16_t)0x0800)  /*!< Pin 11 selected   */
+#define GPIO_PIN_12     ((uint16_t)0x1000)  /*!< Pin 12 selected   */
+#define GPIO_PIN_13     ((uint16_t)0x2000)  /*!< Pin 13 selected   */
+#define GPIO_PIN_14     ((uint16_t)0x4000)  /*!< Pin 14 selected   */
+#define GPIO_PIN_15     ((uint16_t)0x8000)  /*!< Pin 15 selected   */
+#define GPIO_PIN_ALL    ((uint16_t)0xFFFF)  /*!< All pins selected */
     
-#define GPIO_MODE_INPUT                 
-#define GPIO_MODE_OUTPUT_PP             
-#define GPIO_MODE_OUTPUT_OD             
-#define GPIO_MODE_AF_PP                 
-#define GPIO_MODE_AF_OD                 
-#define GPIO_MODE_ANALOG                
-#define GPIO_MODE_IT_RISING             
-#define GPIO_MODE_IT_FALLING            
-#define GPIO_MODE_IT_RISING_FALLING     
-#define GPIO_MODE_EVT_RISING            
-#define GPIO_MODE_EVT_FALLING           
-#define GPIO_MODE_EVT_RISING_FALLING    
+#define GPIO_MODE_INPUT                 0x00000000U /*!< Input Floating      */
+#define GPIO_MODE_OUTPUT_PP             0x00000001U /*!< Output PushPull     */
+#define GPIO_MODE_OUTPUT_OD             0x00000011U /*!< Output Open Drain   */
+#define GPIO_MODE_AF_PP                 0x00000002U /*!< Alt. Fun. Push Pull */
+#define GPIO_MODE_AF_OD                 0x00000012U /*!< Alt. Fun. Open Drain*/
+#define GPIO_MODE_ANALOG                0x00000003U /*!< Analog              */
+#define GPIO_MODE_IT_RISING             0x10110000U /*!< Ext. Interrupt with
+rising edge trigger      */
+#define GPIO_MODE_IT_FALLING            0x10210000U /*!< Ext. Interrupt with
+falling edge trigger     */
+#define GPIO_MODE_IT_RISING_FALLING     0x10310000U /*!< Ext. Interrupt with
+rising & falling edge trigger  */
+#define GPIO_MODE_EVT_RISING            0x10120000U /*!< Ext. event with
+rising edge trigger detecting */ 
+#define GPIO_MODE_EVT_FALLING           0x10220000U /*!< Ext. event with
+falling edge trigger detecting */ 
+#define GPIO_MODE_EVT_RISING_FALLING    0x10320000U /*!< Ext. event with
+rising & falling edge trigger detecting */ 
 
-#define GPIO_PULL_NONE  (0x00)
-#define GPIO_PULLUP     (0x01)    
-#define GPIO_PULLDOWN   (0x10)
+#define GPIO_PULL_NONE  0x00000000U /*!< No Pull-up or Pull-down activation  */
+#define GPIO_PULLUP     0x00000001U /*!< Pull-up activation                  */  
+#define GPIO_PULLDOWN   0x00000002U /*!< Pull-down activation                */
 
-#define GPIO_SPEED_LOW
-#define GPIO_SPEED_HIGH
-#define GPIO_SPEED_FAST
+#define GPIO_SPEED_LOW    0x00000000U /*!< Low speed, refer to datasheet     */
+#define GPIO_SPEED_MED    0x00000001U /*!< Medium speed, refer to datasheet  */
+#define GPIO_SPEED_FAST   0x00000002U /*!< Fast speed, refer to datasheet    */
+#define GPIO_SPEED_HIGH   0x00000003U /*!< High speed, refer to datasheet    */
 
 /*****************************************************************************/
 /*****************************************************************************/
