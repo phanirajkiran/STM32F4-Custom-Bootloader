2ca55af39447bd4a841b9f8d3bc275d33024e58c
William Huang <william.huang03@outlook.com>
2017-10-23 07:33:31 -0400

    Added the flash utility

---
+++ CustomBootloaderFlashUtitlity/CustomBootloaderFlash/Models/MainWindowModel.cs
@@ -0,0 +1,168 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CustomBootloaderFlash.Models
+{
+    public class MainWindowModel : BindableBase
+    {
+        #region Private members
+        private SerialPort _serialport;
+        private int[] _baudrates = { 115200 };
+
+        #region Commands to Target
+        /// <summary>
+        /// The erase command
+        /// </summary>
+        private byte[] Command_Erase = { 0x43 };
+        #endregion
+
+        #endregion
+
+        #region Public Fields
+        public int[] BaudRates
+        {
+            get { return _baudrates; }
+        }
+        #endregion
+
+        #region Constructures
+        public MainWindowModel()
+        {
+            _serialport = new SerialPort();
+        }
+
+        ~MainWindowModel()
+        {
+            Target_Disconnect();
+        }
+        #endregion
+
+        #region Private fuctions
+        /// <summary>
+        /// Data received from the target
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Target_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            int receivedData = sp.ReadByte();
+        }
+        /// <summary>
+        /// Retrieves the available com ports in the system
+        /// </summary>
+        public ObservableCollection<string> GetComPorts()
+        {
+            return new ObservableCollection<string>(SerialPort.GetPortNames());
+        }
+
+        /// <summary>
+        /// Sends a command to the target
+        /// </summary>
+        /// <param name="command_msg"></param>
+        private void Target_SendCommand(byte[] command_msg)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns a 1-byte checksum of the message.
+        /// The first byte of the message is XOR with a value of 0xFF.
+        /// Subsequent messages are XOR with the previous checksum value.
+        /// The result is then returned.
+        /// </summary>
+        /// <param name="message">The message to create the checksum from</param>
+        /// <returns>The checksum of the message</returns>
+        private byte GetChecksum(byte[] message)
+        {
+            byte checksum = 0xFF;
+
+            for(int i = 0; i < message.Length; i++)
+            {
+                checksum ^= message[i];
+            }
+
+            return checksum;
+        }
+        #endregion
+
+        #region Public Functions
+        /// <summary>
+        /// Connect to the serial port with the specified comport and baudrate.
+        /// All other settigns are default:
+        /// No parity, one stopbit, 8 data bits, no flow control.
+        /// </summary>
+        /// <param name="comport">The name of the COM port to connect to</param>
+        /// <param name="baudrate">The baud rate to use for communication</param>
+        /// <returns>True if connection is succesfful, otherwise returns false</returns>
+        public bool Target_Connect(string comport, int baudrate)
+        {
+            _serialport.PortName = comport;
+            _serialport.BaudRate = baudrate;
+            _serialport.Parity = Parity.None;
+            _serialport.StopBits = StopBits.One;
+            _serialport.DataBits = 8;
+            _serialport.Handshake = Handshake.None;
+            _serialport.RtsEnable = false;
+            _serialport.DataReceived += new SerialDataReceivedEventHandler(Target_DataReceived);
+            try
+            {
+                _serialport.Open();
+                Target_Erase(0x01);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Disconnect from the target
+        /// </summary>
+        public void Target_Disconnect()
+        {
+            if (_serialport.IsOpen)
+                _serialport.Close();
+        }
+
+        /// <summary>
+        /// Checks to see if the file size fits within the flash size of the target
+        /// </summary>
+        /// <returns></returns>
+        public bool Flash_CheckSize(long filesize)
+        {
+            bool result = false;
+
+            if (filesize <= 480)
+                result = true;
+
+            return result;
+        }
+
+        public void Target_Erase(byte sectors)
+        {
+            int length = Command_Erase.Length;
+            byte[] msg = new byte[length + 1];
+            for (int i = 0; i < length; i++)
+                msg[i] = Command_Erase[i];
+
+            msg[length] = GetChecksum(Command_Erase);
+            
+            _serialport.Write(msg, 0, msg.Length);
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ CustomBootloaderFlashUtitlity/CustomBootloaderFlash/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,248 @@
+﻿using Prism.Mvvm;
+using CustomBootloaderFlash.Models;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using Prism.Commands;
+using System;
+using System.Windows;
+using Microsoft.Win32;
+using System.IO;
+
+namespace CustomBootloaderFlash.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        #region Private Members
+        /// <summary>
+        /// Title of the main window
+        /// </summary>
+        private string _title = "Custom Bootloader Flash Utility by William Huang";
+
+        #region Connect Button
+        /// <summary>
+        /// String to be shown on the Connect button
+        /// </summary>
+        private string _button_Connect_Text = "Connect!";
+
+        /// <summary>
+        /// Whether the connect button is enabled or not 
+        /// </summary>
+        private bool _button_Connect_IsEnabled = true;
+        #endregion
+
+        /// <summary>
+        /// The Selected Comport Value
+        /// </summary>
+        private string _selectedComPort;
+
+        private string _filepath;
+        /// <summary>
+        /// the main window model
+        /// </summary>
+        private MainWindowModel _mainWindowModel;
+
+        #region Progress Bar
+        /// <summary>
+        /// The maximum value for the progress bar
+        /// </summary>
+        private long _progressbar_maximum = 256;
+        /// <summary>
+        /// the minimum value for the progress bar
+        /// </summary>
+        private long _progressbar_minimum = 0;
+
+        /// <summary>
+        /// the current value of the progress bar
+        /// </summary>
+        private long _progressbar_current = 0;
+        #endregion
+        #endregion
+
+        #region Public Fields
+        /// <summary>
+        /// Title of the main window
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region Connect Button
+        /// <summary>
+        /// String to be shown on the Connect button
+        /// </summary>
+        public string Button_Connect_Text
+        {
+            get { return _button_Connect_Text; }
+            set { SetProperty(ref _button_Connect_Text, value); }
+        }
+
+        /// <summary>
+        /// Whether the connect button is enabled or not 
+        /// </summary>
+        public bool Button_Connect_IsEnabled
+        {
+            get { return _button_Connect_IsEnabled; }
+            set
+            {
+                SetProperty(ref _button_Connect_IsEnabled, value);
+
+            }
+        }
+
+        /// <summary>
+        /// Command for the connect button
+        /// </summary>
+        public DelegateCommand Connect_Command { get; private set; }
+        #endregion
+
+        #region Comport and Baud Rate
+        /// <summary>
+        /// List of the available com ports
+        /// </summary>
+        public ObservableCollection<string> ComPorts { get; set; } = new ObservableCollection<string>();
+        /// <summary>
+        /// List of the available baud rates
+        /// </summary>
+        public ObservableCollection<int> BaudRates { get; set; }
+        /// <summary>
+        /// The Selected Baud Rate
+        /// </summary>
+        public int SelectedBaudRate { get; set; }
+
+        public string SelectedComPort
+        {
+            get { return _selectedComPort; }
+            set
+            {
+                SetProperty(ref _selectedComPort, value);
+                Connect_Command.RaiseCanExecuteChanged();
+            }
+        }
+        #endregion
+
+        #region Progress Bar
+        /// <summary>
+        /// the max value for the progress bar
+        /// </summary>
+        public long ProgressBar_Maximum
+        {
+            get { return _progressbar_maximum; }
+            set { SetProperty(ref _progressbar_maximum, value); }
+        }
+
+        /// <summary>
+        /// the minimum value for the progress bar
+        /// </summary>
+        public long ProgressBar_Minimum
+        {
+            get { return _progressbar_minimum; }
+            set { SetProperty(ref _progressbar_minimum, value); }
+        }
+        /// <summary>
+        /// the current value of the progress bar
+        /// </summary>
+        public long ProgressBar_Current
+        {
+            get { return _progressbar_current; }    
+            set { SetProperty(ref _progressbar_current, value); }
+        }
+        #endregion
+
+        #region Browse File
+        /// <summary>
+        /// Delegate command for the browse file
+        /// </summary>
+        public DelegateCommand BrowseFile_Command { get; private set; }
+        /// <summary>
+        /// Whether or not the browse file command is enabled
+        /// </summary>
+        public bool BrowseFile_IsEnabled { get; set; } = true;
+        /// <summary>
+        /// The file path of the selected file
+        /// </summary>
+        public string FilePath
+        {
+            get { return _filepath; }
+            private set
+            {
+                SetProperty(ref _filepath, value);
+                UpdateFileSize();
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// the default constructor
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            _mainWindowModel = new MainWindowModel();
+            ComPorts = _mainWindowModel.GetComPorts();
+            BaudRates = new ObservableCollection<int>(_mainWindowModel.BaudRates);
+
+            Connect_Command = new DelegateCommand(Connect_CommandExecute, Connect_CommandCanExecute);
+            BrowseFile_Command = new DelegateCommand(BrowseFile_CommandExecute).ObservesCanExecute(() => BrowseFile_IsEnabled);
+
+        }
+
+        
+        #endregion
+
+        #region Private functions
+        #region Connect Button
+        private void Connect_CommandExecute()
+        {
+            if (_mainWindowModel.Target_Connect(SelectedComPort, SelectedBaudRate))
+            {
+                MessageBox.Show("Connection Successful!");
+            }
+            else
+            {
+                MessageBox.Show("Unable to connect to target");
+            }
+        }
+
+        private bool Connect_CommandCanExecute()
+        {
+
+            if (String.IsNullOrEmpty(SelectedComPort))
+                Button_Connect_IsEnabled = false;
+            else
+                Button_Connect_IsEnabled = true;
+
+            return Button_Connect_IsEnabled;
+        }
+        #endregion
+
+        #region Browse File Button
+        private void BrowseFile_CommandExecute()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog()
+            {
+                Filter = ".bin | *.bin"
+            };
+
+            if(fileDialog.ShowDialog() == true)
+            {
+                FilePath = fileDialog.FileName;
+            }
+        }
+
+        private void UpdateFileSize()
+        {
+            FileInfo fileInfo = new FileInfo(FilePath);
+            string filesize= fileInfo.Length.ToString();
+
+            filesize = string.Format("{0}", fileInfo.Length >> 10);
+            ProgressBar_Maximum = (fileInfo.Length >> 10);
+        }
+        #endregion
+        #endregion
+
+    }
+}
